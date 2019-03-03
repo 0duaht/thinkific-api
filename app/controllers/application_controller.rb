95bbb5cb34cd37e1452b0a0bc79a3c23b50093d6
Tobi Oduah <tobi.oduah@andela.com>
2019-03-03 01:35:18 +0100

    Render README in root URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def index
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    @markdown_content = markdown.render(File.open(Rails.root + 'README.md', 'r').read)
+  end
 end
